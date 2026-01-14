48ee3d2f820ef8e999156584218ffb858d52787a
Angel Lopez <srterradev@gmail.com>
2026-01-13 22:19:28 -0600

    feat: footer refactor

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,10 +1,10 @@
 'use client';
 
-import { Center, Flex, HStack, Text, VStack } from '@chakra-ui/react';
+import { Center, Flex, Text, VStack } from '@chakra-ui/react';
 import tinycolor from 'tinycolor2';
+import { BiLogoGithub } from 'react-icons/bi';
 import useSessionStore from '@/stores/Session.store';
 import usePomodoroStore from '@/stores/Pomodoro.store';
-import Image from 'next/image';
 import Link from 'next/link';
 import { useTranslations } from 'use-intl';
 
@@ -26,42 +26,30 @@ export const Footer = () => {
       paddingY={12}
       gap={4}
     >
-      <VStack>
-        <a
-          style={{ maxWidth: 200, marginBottom: 20 }}
-          href='https://www.buymeacoffee.com/srterra'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          <Image
-            width={200}
-            height={50}
-            src='https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png'
-            alt='Buy Me A Coffee'
-          />
-        </a>
-
-        <Text fontSize='sm' color='gray.500'>
-          &copy; {new Date().getFullYear()} {t('copyright')}
-        </Text>
-
-        <Text fontSize='sm' fontWeight='bold' color='gray.500'>
-          {t('madeWith')}
-        </Text>
-      </VStack>
-
       <Center>
         <Text fontSize='sm' color='gray.500'>
           {t('disclaimer')}
         </Text>
       </Center>
 
-      <HStack gap={8} marginTop={4} fontSize='sm' color='gray.500'>
-        <Link href='/about-us'>{t('links.about')}</Link>
-        <Link href='/terms'>{t('links.terms')}</Link>
-        <Link href='/privacy'>{t('links.privacy')}</Link>
-        <Link href='/legal'>{t('links.legal')}</Link>
-      </HStack>
+      <VStack>
+        <Text fontSize='sm' fontWeight='bold' color='gray.500'>
+          {t('madeWith')}
+        </Text>
+
+        <Flex alignItems='center' gap={2}>
+          <BiLogoGithub />
+          <Link
+            className={'cursor-pointer link underline'}
+            target={'_blank'}
+            href={'https://github.com/srteerra/pitmydoro'}
+          >
+            <Text fontSize='sm' fontWeight='medium' color='gray.800'>
+              Contributions are welcome!
+            </Text>
+          </Link>
+        </Flex>
+      </VStack>
     </Flex>
   );
 };

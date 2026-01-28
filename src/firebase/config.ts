df4cc336458a76401db89175331a25a4053ae564
Angel Lopez <srterradev@gmail.com>
2026-01-27 22:16:18 -0600

    refactor: firebase removed

--- src/firebase/config.ts
+++
@@ -1,8 +0,0 @@
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
-import { environment } from '@/environments/environment.dev';
-
-const app = initializeApp(environment.firebase);
-const db = getFirestore(app);
-
-export { app, db };
